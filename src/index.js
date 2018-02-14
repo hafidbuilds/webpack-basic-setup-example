8a1dd623e6823a750e4734cc37f37bbb28fe88a0
hafidz <hafidzzpermana09@gmail.com>
2018-02-14 23:21:46 +0700

    update route API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Match, Miss } from 'react-router';
+import { BrowserRouter , Route, Switch } from 'react-router-dom';
 
 import './css/style.css';
 import App from './components/App';
@@ -10,11 +10,13 @@ import NotFound from './components/NotFound';
 const Root = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Match exactly pattern="/" component={StorePicker} />
-        <Match pattern="/store/:storeId" component={App} />
-        <Miss component={NotFound} />
-      </div>
+    <div>
+        <Switch>
+          <Route exact path="/" component={StorePicker} />
+          <Route path="/store/:storeId" component={App} />
+          <Route component={NotFound} />
+        </Switch>
+        </div>
     </BrowserRouter>
   )
 }
